5014b2f2eb69cb58c4242d191394fec54e186735
João Paulo <joaopaulojr10@gmail.com>
2026-05-11 21:58:23 -0300

    feat(app): instala primitives shadcn (14 componentes) + TooltipProvider/Toaster

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,9 +3,14 @@ import { createRoot } from 'react-dom/client'
 import '@fontsource-variable/inter/index.css'
 import './index.css'
 import App from './App.tsx'
+import { TooltipProvider } from '@/components/ui/tooltip'
+import { Toaster } from '@/components/ui/sonner'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TooltipProvider>
+      <App />
+      <Toaster position="bottom-right" />
+    </TooltipProvider>
   </StrictMode>,
 )
